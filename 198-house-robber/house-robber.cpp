b5d0ccb8b50b62de5de36bfcecbfbd72c9a1ec81
Aman Trivedi <133767080+aman4374@users.noreply.github.com>
2025-03-28 14:17:27 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (44.61%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,23 +1,35 @@
 class Solution {
 public:
-    int solve(vector<int> &nums, int n,vector<int> &dp) {
-    if(n < 0) return 0;
-    if(n == 0) return nums[0];
+    // int solve(vector<int> &nums, int n,vector<int> &dp) {
+    // if(n < 0) return 0;
+    // if(n == 0) return nums[0];
 
-    if(dp[n]!=-1) return dp[n];
+    // if(dp[n]!=-1) return dp[n];
 
-    int incl = solve(nums, n - 2,dp) + nums[n];
-    int excl = solve(nums, n - 1,dp);
+    // int incl = solve(nums, n - 2,dp) + nums[n];
+    // int excl = solve(nums, n - 1,dp);
 
-    dp[n] = max(incl, excl);
-    return dp[n];
-}
-    int rob(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> dp(n,-1);
-        int ans = solve(nums,n-1,dp);
-
-        return ans;
+    // dp[n] = max(incl, excl);
+    // return dp[n];
+//}
+    int rob(vector<int>& arr) {
     
+        int n = arr.size();
+
+        if(n<0) return 0;
+        if (n == 1) return arr[0]; // Handle single house case
+
+        vector<int> dp(n, 0);
+        
+        dp[0] = arr[0];
+        dp[1] = max(arr[0], arr[1]);
+
+        for (int i = 2; i < n; i++) {
+            int incl = dp[i-2] + arr[i];
+            int excl = dp[i-1];
+
+            dp[i] = max(incl, excl);
+        }
+        return dp[n-1];
     }
 };
