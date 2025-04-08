9bc1c5a09666a9c997132a9d3cd19462cfb1bec0
Aman Trivedi <133767080+aman4374@users.noreply.github.com>
2025-04-08 11:05:38 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (41.29%) - LeetSync

---
+++ 27-remove-element/remove-element.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int n = nums.size();
+        int ans = 0; // index for placing non-val elements
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != val) {
+                nums[ans] = nums[i];
+                ans++;
+            }
+        }
+        return ans;
+        
+    }
+};
