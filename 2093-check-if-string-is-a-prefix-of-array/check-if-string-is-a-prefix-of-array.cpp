356711ffab728b15ed77bd1b3fe564ceca125564
Aman Trivedi <133767080+aman4374@users.noreply.github.com>
2025-05-11 08:36:11 +0530

    Time: 0 ms (100.00%) | Memory: 17 MB (34.82%) - LeetSync

---
+++ 2093-check-if-string-is-a-prefix-of-array/check-if-string-is-a-prefix-of-array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isPrefixString(string s, vector<string>& words) {
+        string prefix = "";
+
+        for(string word : words){
+            prefix +=word;
+            if(prefix == s) return true;
+            if(prefix.length() > s.length()) return false;
+        }
+        return false;
+    }
+};
