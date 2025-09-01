6c70fc644949ab70254a9e964b6cc3cbc5314015
Aman Trivedi <133767080+aman4374@users.noreply.github.com>
2025-09-01 21:14:11 +0530

    Time: 0 ms (100.00%) | Memory: 18.6 MB (51.45%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,28 +1,27 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-       
-int n = matrix.size();
-int m = matrix[0].size();
-vector<int> row(n,0),col(m,0);
+        
+        int n = matrix.size();
+        int m = matrix[0].size();
 
-    // Traverse the matrix:
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            if (matrix[i][j] == 0) {
-                row[i] = 1;
-                col[j] = 1;
+        vector<int> row(n,0),col(m,0);
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(matrix[i][j] == 0){
+                    row[i] = 1;
+                    col[j] = 1;
+                }
             }
         }
-    }
-    // Finally, mark all (i, j) as 0
-    // if row[i] or col[j] is marked with 1.
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            if (row[i] || col[j]) {
-                matrix[i][j] = 0;
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(row[i] || col[j]){
+                    matrix[i][j] = 0;
+                }
             }
         }
-    }
     }
 };
