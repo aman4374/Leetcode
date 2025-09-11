bb3ac249daeaf1e27a0b6a6a0090be3f09070557
Aman Trivedi <133767080+aman4374@users.noreply.github.com>
2025-09-11 23:53:04 +0530

    Time: 215 ms (5.09%) | Memory: 14.1 MB (63.77%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -4,8 +4,8 @@ public:
         int n = nums.size();
 
         for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                
+            for(int j=0;j<n;j++){
+                if(i == j) continue;
                 if(nums[i]+nums[j] == target){
                     return{i,j};
                 }
